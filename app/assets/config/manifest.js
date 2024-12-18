a5b8218114a7c40a4ca9698d078599eca67be763
Nikhil Dronamraju <nikhil.dronamraju.0917@gmail.com>
2024-12-18 07:59:40 -0800

    Done with about me

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= link_directory ../files
+//= link_directory ../javascripts
+//= link_directory ../stylesheets//= link_tree ../builds
